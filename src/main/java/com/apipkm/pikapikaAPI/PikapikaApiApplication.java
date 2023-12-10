432cc12f8348c5a2a0131b0eb712056186b3324e
Emanuel <esotomayor@sms-sudamerica.com>
2023-12-10 02:02:55 -0300

    Se agrega proyecto inicial

---
+++ src/main/java/com/apipkm/pikapikaAPI/PikapikaApiApplication.java
@@ -0,0 +1,13 @@
+package com.apipkm.pikapikaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PikapikaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PikapikaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apipkm/pikapikaAPI/PikapikaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apipkm.pikapikaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PikapikaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
